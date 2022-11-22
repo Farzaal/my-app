931160c0de5ee324112a77af6fb7ba7bbf9cad9a
farzal-locofy <farzal@locofy.ai>
2022-11-22 17:38:49 +0500

    BasicTooltip

---
+++ src/BasicTooltip.js
@@ -0,0 +1,9 @@
+import React from 'reactttt';
+import { Tooltip } from 'antd';
+
+const BasicTooltip = () => (
+  <Tooltip title="prompt text">
+    <span>Tooltip will show on mouse enter.</span>
+  </Tooltip>
+);
+export default BasicTooltip;
