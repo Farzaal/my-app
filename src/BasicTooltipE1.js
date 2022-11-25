9f0778ba319b1b170b71b568f5a467a909d651ec
farzal-locofy <farzal@locofy.ai>
2022-11-25 11:40:38 +0500

    G1 commit

--- src/BasicTooltipE1.js
+++ src/BasicTooltipE1.js
@@ -1,15 +1,22 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 
 // git merge origin/github-diff-demo
 export default function BasicTooltipE1() {
+
+  let [dogImage, setDogImage] = useState(null)
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => setDogImage(data.message))
+  },[])
+
   return (
-    <Tooltip title="Delete">
-      <IconButton>
-        <DeleteIcon />
-      </IconButton>
-    </Tooltip>
+    <div className='tariff'>
+      <img src={dogImage} />
+    </div>
   );
 }
